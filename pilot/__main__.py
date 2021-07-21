b09b81daa4d8ad3bd7c13fc3c110080e630c2d82
AnthonySDi <stenbergdigeronimo@gmail.com>
2021-07-20 23:03:46 -0700

    Update __main__.py
    Deleted old code and added doc strings to main and four new functions:
    
    - journal: takes time of day and calls helper functions morning() or evening()
    - morning: prompts user for responses for a morning journal entry. Calls helper function grateful()
    - evening: prompts user for responses for a evening journal entry. Calls helper function grateful()
    - grateful: helper function, asks users for what they're grateful for and is called on by morning() and evening()

--- pilot/__main__.py
+++ pilot/__main__.py
@@ -3,10 +3,20 @@ import logging
 import datetime
 #from .classmodule import MyClass
 #from .funcmodule import my_function
+logging.basicConfig(filename='develop.log', encoding='utf-8', level=logging.DEBUG)
 
 def main():
-    start = datetime.datetime.now()
-    stop = ''
+    '''
+    - Main is evoked by the keyword 'pilot' at the command line. The keyword and anything after it will be space delimited and stored in an array, which can be accessed by calling sys.argv[1:]
+
+    KEYWORD: pilot
+
+    Params:
+        am - starts morning journal
+        pm - starts night journal
+    '''
+    logging.info('Keyword evoked and Main start')
+    
     timeDifference = ''
     print('in main')
     args = sys.argv[1:]
@@ -16,50 +26,64 @@ def main():
     # my_function('hello world')
     
     print(args[0])
-#    if (args[0].lower() == 'read'):
-#        stop = readAction(args)
-#    elif (args[0].lower() == 'write'):
-#        stop = writeAction(args)
-#    else:
-#        print('Command not found read or write will be accepted')
+    logging.info('Main function ended')
 
-#    timeDifference = stop - start
+def journal(tod):
+    '''
+    calls helper functions morning, and evening
 
-#    print("Total time took ")
-#    print(timeDifference)
+    @params
+    tod string; will represent Time of Day, am or pm
 
-def readAction(args):
-    print("Read Action Function accessed")
-    fileName = open("stock.txt", "r")
-    sentence = fileName.readline()
-    print(sentence)
-    count = 0
-    index = 0
-    print("**Index is ")
-    print(index)
-    print('*****************')
-    print("**len(sentence) is ")
-    print(len(sentence))
-    print('*************')
-    while index < len(sentence):
-        index = sentence.find('imperdiet', index)
-        
-        if index != -1:
-            print('imperdiet', index)
-            count += 1
-            index += 9
+    '''
+    logging.info('Journal function started')
+    if tod != 'am':
+        morning()
+    else:
+        evening()
+    logging.info('Journal function ended')
+
+def morning():
+    '''
+    # Morning 
+    - I am grateful for: (three lines)
+    - What would make today great? (three lines)
+    - Daily affirmations
+    - my goal
+    - My targets (3)
+    - Schedule
+    '''
+    logging.info('Morning function started')
+
+    logging.info('Morning function ended')
+
+def evening():
+    '''
+    # Night
+    - 3 amazing things that happened today
+    - How could I have made today even better? (three lines)
+    - Lessons learned (3)
+    - wins(3)
+    - I am grateful for: (three lines)
+    '''
+    logging.info('Evening function started')
+
+    logging.info('Evening function ended')
+
+def grateful(tod):
+    '''
+    asks user for three things they are grateful for, stores them in an array which is what it returns
+    '''
+    logging.info('Grateful function started')
+    things = []
+
+    while len(things) < 3:
+        if tod != 'am':
+            things.append(raw_input('What are you grateful for this morning gorgeous?'))
         else:
-            break
+            things.append(raw_input('What are you grateful for this evening gorgeous?'))
 
-    fileName.close()
-    return datetime.datetime.now()
-
-    
-def writeAction(arges):
-    fileName = open("stock.txt", "r")
-
-    fileName.close()
-    return datetime.datetime.now()
+    logging.info('Grateful function ended')
 
 if __name__ == '__main__':
     main()
